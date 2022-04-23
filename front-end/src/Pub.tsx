d42dbebb00156b6737a7cfd4e51a0833326491be
Chuanyuan <liuchuanyuan@gmail.com>
2022-04-23 10:02:04 +1000

    Enabled Markers

--- front-end/src/Pub.tsx
+++ front-end/src/Pub.tsx
@@ -2,31 +2,23 @@ import React, { useEffect, useMemo, useState } from "react";
 import { Button, Layout } from 'antd';
 import { Typography } from 'antd';
 import { CameraOutlined, SettingOutlined } from '@ant-design/icons'
-import { GoogleMap, useLoadScript, Marker, OverlayView } from "@react-google-maps/api"
+import { GoogleMap, useLoadScript, Marker as MarkerImpl, OverlayView } from "@react-google-maps/api"
 const { Header, Content, Footer } = Layout;
 const { Title } = Typography;
 
 const defaultPos = { lat: -37.797624159993, lng: 144.95950740721196 }
-const iconBase = "https://developers.google.com/maps/documentation/javascript/examples/full/images/"
-
-const icons: Record<string, { icon: string }> = {
-    parking: {
-        icon: iconBase + "parking_lot_maps.png",
-    },
-    library: {
-        icon: iconBase + "library_maps.png",
-    },
-    info: {
-        icon: iconBase + "info-i_maps.png",
-    },
-};
-
 
 function Map() {
     const [myPos, setMyPos] = useState<google.maps.LatLngLiteral>(defaultPos)
     const [center, setCenter] = useState<google.maps.LatLngLiteral>(defaultPos)
+    const Person = () => (
+        <MarkerImpl position={myPos} icon="http://localhost:3000/person.png" />
+    );
+    const Bin = () => (
+        <MarkerImpl position={center} icon="http://localhost:3000/bin.png" />
+    );
     useEffect(() => {
-        navigator.geolocation.getCurrentPosition(
+        navigator.geolocation.watchPosition(
             ({ coords }) => {
                 console.log('myPos', coords.latitude, coords.longitude)
                 setMyPos({ lat: coords.latitude, lng: coords.longitude })
@@ -44,7 +36,8 @@ function Map() {
         )
     }, [])
     return <GoogleMap zoom={17} center={center} mapContainerClassName="map-container">
-        <Marker position={myPos} icon={icons.parking.icon} />
+        <Bin/>
+        <Person/>
     </GoogleMap>
 }
 
