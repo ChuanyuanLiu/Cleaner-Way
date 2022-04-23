4b25cfebd6486ac77f5e978ff0d499006fede6a1
Chuanyuan <liuchuanyuan@gmail.com>
2022-04-23 14:33:29 +1000

    Public WIP

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -6,6 +6,24 @@ import Gov from './Gov';
 import Login from './Login';
 import Pub from './Pub';
 
+// Import the functions you need from the SDKs you need
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB3Z2w1kTnntV9Irug14o2cQms7bmyhf40",
+  authDomain: "clearway-db4d4.firebaseapp.com",
+  projectId: "clearway-db4d4",
+  storageBucket: "clearway-db4d4.appspot.com",
+  messagingSenderId: "925241230126",
+  appId: "1:925241230126:web:5d5d0edc85433d8f36f0bf",
+  measurementId: "G-TZQW4ES09W"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
 function App() {
     return (
         <Router>
--- front-end/src/Pub.tsx
+++ front-end/src/Pub.tsx
@@ -9,17 +9,95 @@ const { Title } = Typography;
 const defaultPos = { lat: -37.797624159993, lng: 144.95950740721196 }
 
 interface MapInterface {
-  showModal: () => void;
+    showModal: () => void;
+    setId: (arg: number) => void;
 }
 
-function Map({showModal} : MapInterface) {
+interface User {
+    diamond: number
+}
+
+enum GarbageType {
+    HouseholdWaste = 0,
+    HardWaste,
+    HaxardousWaste,
+    OrganicWaste,
+    NotSure
+}
+
+enum StillThereType {
+    Yes = 0,
+    No,
+    Unsure,
+}
+
+enum StatusType {
+    WaitingToBeProcessed = 0,
+    PickupOnTheWay,
+    PickupCompleted
+}
+
+interface GarbageInterface {
+    stillThere: { type: StillThereType, date: Date }[],
+    numberReported: number,
+    type: GarbageType[],
+    photo: string,
+    date: Date,
+    location: google.maps.LatLngLiteral,
+    status: StatusType,
+    id: number
+}
+
+const garbages: GarbageInterface[] = [
+    {
+        stillThere: [
+            { type: StillThereType.Yes, date: new Date() },
+        ],
+        numberReported: 5,
+        type: [GarbageType.OrganicWaste],
+        photo: "http://localhost:3000",
+        date: new Date(),
+        location: { lat: -37.797624159993, lng: 144.95950740721196 },
+        status: StatusType.PickupOnTheWay,
+        id: 1
+    },
+    {
+        stillThere: [
+            { type: StillThereType.No, date: new Date() },
+            { type: StillThereType.Yes, date: new Date() },
+        ],
+        numberReported: 1,
+        type: [GarbageType.HardWaste],
+        photo: "http://localhost:3000",
+        date: new Date(),
+        location: { lat: -37.792624159993, lng: 144.95250740721196 },
+        status: StatusType.WaitingToBeProcessed,
+        id: 2
+    },
+    {
+        stillThere: [
+            { type: StillThereType.Yes, date: new Date() },
+            { type: StillThereType.No, date: new Date() },
+        ],
+        numberReported: 8,
+        type: [GarbageType.HouseholdWaste],
+        photo: "http://localhost:3000",
+        date: new Date(),
+        location: { lat: -37.793624159993, lng: 144.96050740721196 },
+        status: StatusType.PickupCompleted,
+        id: 3
+    }
+]
+
+
+function Map({ showModal, setId }: MapInterface) {
     const [myPos, setMyPos] = useState<google.maps.LatLngLiteral>(defaultPos)
     const [center, setCenter] = useState<google.maps.LatLngLiteral>(defaultPos)
     const Person = () => (
-        <MarkerImpl position={myPos} icon="http://localhost:3000/person.png" onClick={showModal}/>
+        <MarkerImpl position={myPos} zIndex={10} icon="http://localhost:3000/person.png" />
     );
-    const Bin = () => (
-        <MarkerImpl position={center} icon="http://localhost:3000/bin.png" onClick={showModal}/>
+    const Bin = (position: google.maps.LatLngLiteral, id: number, setId: (arg: number) => void) => (
+        <MarkerImpl key={id.toString()} position={position} icon="http://localhost:3000/bin.png" onClick={() => { showModal(); setId(id)} } />
     );
     useEffect(() => {
         navigator.geolocation.watchPosition(
@@ -40,60 +118,58 @@ function Map({showModal} : MapInterface) {
         )
     }, [])
     return <GoogleMap zoom={17} center={center} mapContainerClassName="map-container">
-        <Bin/>
-        <Person/>
+        {garbages.map(({ location, id }) => Bin(location, id, setId))}
+        <Person />
     </GoogleMap>
 }
 
+interface GarbageModalInterface {
+    id: number,
+    isModalVisible: boolean, 
+    setIsModalVisible: (arg: boolean) => void,
+}
+
+function GarbageModal({isModalVisible,setIsModalVisible, id}: GarbageModalInterface) {
+    const data : GarbageInterface = garbages.filter( (data) => data.id = id )[0]
+    return <Modal
+        title="Basic Modal"
+        visible={isModalVisible}
+        onOk={() => setIsModalVisible(false)}
+        onCancel={() => setIsModalVisible(false)}
+    >
+        <p> {data.photo} </p>
+    </Modal>
+}
+
 export default function Pub() {
-  const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY ?? "",
-  });
+    const { isLoaded, loadError } = useLoadScript({
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY ?? "",
+    });
 
-  const [isModalVisible, setIsModalVisible] = useState(false);
+    const [id, setId] = useState<number>(1);
+    const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
 
-  const showModal = () => {
-    setIsModalVisible(true);
-  };
-
-  const handleOk = () => {
-    setIsModalVisible(false);
-  };
-
-  const handleCancel = () => {
-    setIsModalVisible(false);
-  };
-
-  return (
-    <Layout style={{ height: "100vh" }}>
-      <Header className="Header">
-        <Title> Waste Collection </Title>
-      </Header>
-      <Content>
-        <Modal
-          title="Basic Modal"
-          visible={isModalVisible}
-          onOk={handleOk}
-          onCancel={handleCancel}
-        >
-          <p>Some contents...</p>
-          <p>Some contents...</p>
-          <p>Some contents...</p>
-        </Modal>
-        {isLoaded ? <Map showModal={showModal} /> : <div>Loading...</div>}
-        {loadError && <div>Map cannot be loaded right now, sorry.</div>}
-      </Content>
-      <Footer className="Footer">
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <Button> 💎 </Button>
-          <Button>
-            <CameraOutlined />
-          </Button>
-          <Button>
-            <SettingOutlined />
-          </Button>
-        </div>
-      </Footer>
-    </Layout>
-  );
+    return (
+        <Layout style={{ height: "100vh" }}>
+            <Header className="Header">
+                <Title> Waste Collection </Title>
+            </Header>
+            <Content>
+                <GarbageModal id={id} setIsModalVisible={setIsModalVisible} isModalVisible={isModalVisible} />
+                {isLoaded ? <Map showModal={() => setIsModalVisible(true)} setId={setId} /> : <div>Loading...</div>}
+                {loadError && <div>Map cannot be loaded right now, sorry.</div>}
+            </Content>
+            <Footer className="Footer">
+                <div style={{ display: "flex", justifyContent: "center" }}>
+                    <Button> 💎 </Button>
+                    <Button>
+                        <CameraOutlined />
+                    </Button>
+                    <Button>
+                        <SettingOutlined />
+                    </Button>
+                </div>
+            </Footer>
+        </Layout>
+    );
 }
